e16d426f5c4797076879fd21aab459dc5cac9caa
williebo11 <will.page2004@hotmail.co.uk>
2026-02-25 17:45:18 +0000

    Update fights.js

--- functions/api/fights.js
+++ functions/api/fights.js
@@ -24,7 +24,7 @@ export async function onRequestGet(context) {
   const day   = String(now.getDate()).padStart(2, '0');
 
   // Correct URL format: summaries.json with api_key as query param
-  const MMA_URL = `https://api.sportradar.com/mma/trial/v2/en/seasons/sr:season:138002/schedules.json?api_key=${SPORTRADAR_KEY}`;
+  const MMA_URL = `https://api.sportradar.com/mma/trial/v2/en/seasons/sr:season:138002/schedule.json?api_key=${SPORTRADAR_KEY}`;
   const [boxingRes, mmaRes] = await Promise.all([
     safeFetch(BOXING_API, {
       headers: {
